19eb968705bf126b0591288c360d516d6846a406
Yuriy Sogokon <ysogokon@honovi.com>
2021-03-16 11:00:43 -0400

    End of section 10
    Signed-off-by: Yuriy Sogokon <ysogokon@honovi.com>

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -37,9 +37,9 @@ export class MemberEditComponent implements OnInit {
   }
 
   updataMember() {
-    console.log(this.member);
-    this.toastr.success('Profile update successfully');
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm.reset(this.member);
+    })
   }
-
 }
