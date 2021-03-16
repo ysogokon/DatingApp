8f9da53310a1f90134e8f2c1807492046c91594d
Yuriy Sogokon <ysogokon@msn.com>
2021-03-15 20:31:17 -0400

    Mid of section 10(118)
    Signed-off-by: Yuriy Sogokon <ysogokon@msn.com>

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChangesGuard implements CanActivate, CanDeactivate<unknown> {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.editForm.dirty){
+      return confirm('Are you sure you want to continue? Unsaved changes will be lost');
+    }
+    return true;
+  }
+  
+}
