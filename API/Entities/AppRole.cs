28ea78c7200fd7f9e70183899d14f5e0a48f924f
Yuriy Sogokon <ysogokon@honovi.com>
2021-04-07 14:57:40 -0400

    End of section 16
    Signed-off-by: Yuriy Sogokon <ysogokon@honovi.com>

---
+++ API/Entities/AppRole.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities
+{
+    public class AppRole : IdentityRole<int>
+    {
+        
+        public ICollection<AppUserRole> UserRoles {get; set;}
+    }
+
+}
